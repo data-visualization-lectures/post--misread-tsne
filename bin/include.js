e8ea43e58c5139f84bf57e72dea449f8419bfb00
Yuichi Yazaki <visualizingjp@gmail.com>
2025-09-19 17:33:13 +0900

    パッケージ修正

--- bin/include.js
+++ bin/include.js
@@ -14,13 +14,12 @@
 
 var fs = require("fs"),
     path = require("path"),
-    jsdom = require("jsdom").jsdom,
-    serializeDocument = require("jsdom").serializeDocument;
+    JSDOM = require("jsdom").JSDOM;
 
-let index = jsdom(fs.readFileSync("public/_index.html", "utf8"), {features: {ProcessExternalResources: false, FetchExternalResources: false}});
+let dom = new JSDOM(fs.readFileSync("public/_index.html", "utf8"), {features: {ProcessExternalResources: false, FetchExternalResources: false}});
 
-function renderIncludes(dom) {
-  let includes = [].slice.apply(dom.querySelectorAll("dt-include"));
+function renderIncludes(document) {
+  let includes = [].slice.apply(document.querySelectorAll("dt-include"));
   includes.forEach(function(el) {
     let location = el.getAttribute("src");
     let includeText = fs.readFileSync(path.join("public", location), "utf8");
@@ -29,5 +28,5 @@ function renderIncludes(dom) {
   });
 }
 
-renderIncludes(index);
-process.stdout.write(serializeDocument(index));
+renderIncludes(dom.window.document);
+process.stdout.write(dom.serialize());
